f909eb8b9c8178732b9566ce9759bd418486f51a
bee <rkddbtjd123@gmail.com>
2022-10-29 23:42:41 +0900

    test: Add london style Store::decreaseStock tests

--- london-vs-detroit/src/test/kotlin/me/study/tdd/londonvsdetroit/order/london/StoreTest.kt
+++ london-vs-detroit/src/test/kotlin/me/study/tdd/londonvsdetroit/order/london/StoreTest.kt
@@ -3,6 +3,7 @@ package me.study.tdd.londonvsdetroit.order.london
 import me.study.tdd.londonvsdetroit.order.Product
 import me.study.tdd.londonvsdetroit.order.StoreImpl
 import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 private class StoreTest {
@@ -16,7 +17,32 @@ private class StoreTest {
 
         val stock = store.getStock(givenProduct)
 
-        Assertions.assertThat(stock).isEqualTo(givenStock)
+        assertThat(stock).isEqualTo(givenStock)
+    }
+
+    @Test
+    fun decrease_the_stock_when_stock_is_enough() {
+        val givenProduct = Product.I_PAD
+        val givenStock = (10 .. 20L).random()
+        val givenNumberOfStockReduction = (0 .. 10L).random()
+        val givenProducts = hashMapOf(givenProduct to givenStock)
+        val store = StoreImpl(givenProducts)
+
+        store.decreaseStock(givenProduct, givenNumberOfStockReduction)
+
+        assertThat(store.getStock(givenProduct)).isEqualTo(givenStock - givenNumberOfStockReduction)
+    }
+
+    @Test
+    fun decrease_stock_is_failing_when_the_stock_is_not_enough() {
+        val givenProduct = Product.I_PAD
+        val givenStock = (0 .. 9L).random()
+        val givenNumberOfStockReduction = (10 .. 20L).random()
+        val givenProducts = hashMapOf(givenProduct to givenStock)
+        val store = StoreImpl(givenProducts)
+
+        Assertions.assertThatThrownBy { store.decreaseStock(givenProduct, givenNumberOfStockReduction) }
+            .isInstanceOf(IllegalStateException::class.java)
     }
 
 }
